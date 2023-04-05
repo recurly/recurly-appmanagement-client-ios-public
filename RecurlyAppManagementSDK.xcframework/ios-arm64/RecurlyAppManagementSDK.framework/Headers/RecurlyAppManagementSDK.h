5ec33e0429aa83fca4b35934fcabdddec1b3c40f
George Shoemaker <george.a.shoemaker@gmail.com>
2023-04-05 13:52:58 -0700

    v0.0.1 build

---
+++ RecurlyAppManagementSDK.xcframework/ios-arm64/RecurlyAppManagementSDK.framework/Headers/RecurlyAppManagementSDK.h
@@ -0,0 +1,18 @@
+//
+//  RecurlyAppManagementSDK.h
+//  RecurlyAppManagementSDK
+//
+//  Created by George Shoemaker on 3/27/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecurlyAppManagementSDK.
+FOUNDATION_EXPORT double RecurlyAppManagementSDKVersionNumber;
+
+//! Project version string for RecurlyAppManagementSDK.
+FOUNDATION_EXPORT const unsigned char RecurlyAppManagementSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecurlyAppManagementSDK/PublicHeader.h>
+
+
